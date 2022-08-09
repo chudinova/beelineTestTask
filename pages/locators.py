b6ef0c395246a06304454547be63a5c59a0c9815
Ekaterina Chudinova <ek.chudi@ya.ru>
2022-08-09 15:10:31 +0300

    Locators updated

--- pages/locators.py
+++ pages/locators.py
@@ -2,11 +2,11 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    LOCATOR_ENTER_BUTTON = (By.CSS_SELECTOR, '.MgQFWr.Ynb9Xm.mS4uL4')
-    LOCATOR_PERMANENT_PASSWORD_AUTH_CHOOSE_BUTTON = (By.CSS_SELECTOR, 'div.am0fm7>div>ul>li:nth-child(3)>button')
+    LOCATOR_ENTER_BUTTON = (By.CSS_SELECTOR, 'div.personal button')
+    LOCATOR_PERMANENT_PASSWORD_AUTH_CHOOSE_BUTTON = (By.CSS_SELECTOR, 'div.initial-form div:nth-child(2) li:nth-child(3)')
     LOCATOR_PERMANENT_PASSWORD_AUTH_LOGIN_FIELD = (By.CSS_SELECTOR, '[placeholder="Логин"]')
     LOCATOR_PERMANENT_PASSWORD_AUTH_PASSWORD_FIELD = (By.CSS_SELECTOR, '[placeholder="Пароль"]')
-    LOCATOR_PERMANENT_PASSWORD_AUTH_SUBMIT_BUTTON = (By.CSS_SELECTOR, '.d3jkWo')
+    LOCATOR_PERMANENT_PASSWORD_AUTH_SUBMIT_BUTTON = (By.CSS_SELECTOR, 'div.initial-form__tab-content button[type="submit"]')
 
 
 class IdentityPageLocators:
