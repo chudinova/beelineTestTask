62fc8533924f4404ff301ba9f3c2a2fffd059149
Ekaterina Chudinova <ek.chudi@ya.ru>
2022-08-09 14:00:32 +0300

    Ctn and psw are taken out of the login and password enter methods

--- main_page_test.py
+++ main_page_test.py
@@ -8,8 +8,8 @@ def test_auth_with_permanent_password_and_random_credentials_fail(browser):
     beeline_main_page.go_to_site()
     beeline_main_page.enter_auth_menu()
     beeline_main_page.auth_method_with_permanent_password_choose()
-    beeline_main_page.auth_method_with_permanent_password_login_enter()
-    beeline_main_page.auth_method_with_permanent_password_password_enter()
+    beeline_main_page.auth_method_with_permanent_password_login_enter(beeline_main_page.ctn)
+    beeline_main_page.auth_method_with_permanent_password_password_enter(beeline_main_page.psw)
     beeline_main_page.auth_method_with_permanent_password_submit_button_click()
 
     assert beeline_identity_page.wrong_login_or_password_error_message().is_displayed()
--- pages/main_page.py
+++ pages/main_page.py
@@ -13,18 +13,19 @@ class MainPageLocators:
 
 class MainPageAuth(BasePage):
 
+    ctn = randint(9000000000, 9999999999)
+    psw = randint(000000, 999999)
+
     def enter_auth_menu(self):
         return self.find_element(MainPageLocators.LOCATOR_ENTER_BUTTON).click()
 
     def auth_method_with_permanent_password_choose(self):
         return self.find_element(MainPageLocators.LOCATOR_PERMANENT_PASSWORD_AUTH_CHOOSE_BUTTON).click()
 
-    def auth_method_with_permanent_password_login_enter(self):
-        ctn = randint(9000000000, 9999999999)
+    def auth_method_with_permanent_password_login_enter(self, ctn):
         self.find_element(MainPageLocators.LOCATOR_PERMANENT_PASSWORD_AUTH_LOGIN_FIELD).send_keys(ctn)
 
-    def auth_method_with_permanent_password_password_enter(self):
-        psw = randint(000000, 999999)
+    def auth_method_with_permanent_password_password_enter(self, psw):
         return self.find_element(MainPageLocators.LOCATOR_PERMANENT_PASSWORD_AUTH_PASSWORD_FIELD).send_keys(psw)
 
     def auth_method_with_permanent_password_submit_button_click(self):
