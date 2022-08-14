ba9b4d3bdb8644d4eef6ee65ad5575e560ed4710
Ekaterina Chudinova <ek.chudi@ya.ru>
2022-08-14 18:50:49 +0300

    Enter button locator updated

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    LOCATOR_ENTER_BUTTON = (By.CSS_SELECTOR, 'div.personal button')
+    LOCATOR_ENTER_BUTTON = (By.CSS_SELECTOR, 'div.personal div button')
     LOCATOR_LOGIN_DIALOG_MODAL = (By.CSS_SELECTOR, '[data-component="LegacyModal"]')
     LOCATOR_PERMANENT_PASSWORD_AUTH_CHOOSE_BUTTON = (By.CSS_SELECTOR, 'div.initial-form div:nth-child(2) li:nth-child(3)')
     LOCATOR_PERMANENT_PASSWORD_AUTH_LOGIN_FIELD = (By.CSS_SELECTOR, '[placeholder="Логин"]')
